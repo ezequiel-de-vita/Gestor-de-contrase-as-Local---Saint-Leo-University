eb70eca57849032130bdce03447bfd01c7917012
Ezequiel <ezequielgustavodevita@gmail.com>
2026-02-17 16:42:43 -0300

    Added navigation-compose using version catalog Implemented login and service screens

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation("androidx.navigation:navigation-compose:2.9.7")
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
